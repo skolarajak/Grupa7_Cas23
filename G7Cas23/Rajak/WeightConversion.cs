ae3599d3f70e9662eff1f1a41355e6c72fa934af
Skola Rajak QA <skolarajak.qa@gmail.com>
2020-11-18 22:09:08 +0100

    Add project files.

---
+++ G7Cas23/Rajak/WeightConversion.cs
@@ -0,0 +1,10 @@
+﻿namespace G7Cas23.Rajak
+{
+    class WeightConversion
+    {
+        public static int KilogramsToGrams(int Kilograms)
+        {
+            return Kilograms * 1000;
+        }
+    }
+}
